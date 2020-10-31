9449039d33744aa87b8ac8cbea9fadb3164d6054
Habby Olusesi <habbyolu@gmail.com>
2020-10-31 00:24:14 -0400

    created questions

---
+++ Develop/index.js
@@ -0,0 +1,58 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const questions = [
+    {
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        name: 'description',
+        message: 'Give a description of your project.'
+      },
+      {
+        name: 'installation',
+        message: 'What are the installation instructions?'
+      },
+      {
+        name: 'usage',
+        message: 'What is the usage information?'
+      },
+      {
+        name: 'contributing',
+        message: 'What are the contribution guidelines?'
+      },
+      {
+        name: 'tests',
+        message: 'What are the instructions?'
+      },
+      {
+        name: 'username',
+        message: 'What is your Github username?'
+      },
+      {
+        name: 'email',
+        message: 'What is your email address?'
+      },
+      {
+        type: 'checkbox',
+        message: 'What license would you like?',
+        name: 'license',
+        choices: ['Apache License 2.0', 
+        'GNU General Public License v3.0', 
+        'MIT License', 'BSD 2-Clause "Simplified" License', 
+        'BSD 3-Clause "New" or "Revised" License', 
+        'Boost Software License 1.0', 'Creative Commons Zero v1.0', 
+        'Universal Eclipse Public License 2.0',
+        'GNU Affero General Public License v3.0',
+        'GNU General Public License v2.0',
+        'GNU Lesser General Public License v2.1',
+        'Mozilla Public License 2.0',
+        'The Unlicense']
+      }
+];
+
+init();
